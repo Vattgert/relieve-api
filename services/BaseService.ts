9b367bb7fef14428cc75fee1706f993894c83d8c
IvanTsiupa <ivan@verlocal.com>
2020-12-23 13:59:38 +0200

    incapculated EntityManager and added service error processing method

--- services/BaseService.ts
+++ services/BaseService.ts
@@ -1,16 +1,28 @@
 import { injectable } from 'inversify';
 import { EntityManager, getManager } from 'typeorm';
+import { getMessageFromValidationError, isValidationError } from '../utils/error';
+import { QueryFailedError } from 'typeorm';
 
 @injectable()
 class BaseService{
-    protected entityManager: EntityManager;
+    private entityManager: EntityManager;
 
     constructor(){
         this.entityManager = getManager();
     }
 
-    getManager(): EntityManager{
+    protected getManager(): EntityManager{
         return this.entityManager;
+    }
+
+    protected getProperErrorMessage(error): string{
+        let errorMessage = 'Unexpected error occured';
+        if(isValidationError(error)){   
+            errorMessage = getMessageFromValidationError(error);
+        } else if(error instanceof QueryFailedError){
+            errorMessage = `Error occured while retrieving activities.`;
+        }
+        return errorMessage;
     }
 }
 
