16bdde1163057f48fc4272762156b514cabc2029
IvanTsiupa <ivan@verlocal.com>
2020-12-23 15:31:25 +0200

    added new methods to service interfaces

--- interfaces/services/IVoteService.ts
+++ interfaces/services/IVoteService.ts
@@ -1,3 +1,6 @@
+import { Vote } from "../../models";
+
 export interface IVoteService{
     getVotesCountByUser(userId: string | number): Promise<any>;
+    getVotesByActivity(activityId: string | number): Promise<Vote[]>
 }
