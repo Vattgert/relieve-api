8607cfdf81cd988596d57829b15c3a03d1700ced
IvanTsiupa <ivan@verlocal.com>
2021-04-06 15:03:21 +0300

    code changes in route files according to eslint

--- routes/ActivityRouter.ts
+++ routes/ActivityRouter.ts
@@ -1,46 +1,42 @@
 import { inject, injectable } from 'inversify';
 import { TYPES } from '../di/types';
 import { Controller } from '../interfaces/Controller';
-import { IRouter, Route} from '../interfaces/Route';
+import { IRouter, Route } from '../interfaces/Route';
 
 @injectable()
-class ActivityRouter implements IRouter{
-    private getActivitiesController: Controller;
-    private getActivityController: Controller;
+class ActivityRouter implements IRouter {
+	private getActivitiesController: Controller;
+	private getActivityController: Controller;
 
-    private topPath: string;
+	private topPath: string;
 
-    constructor(
-        @inject(TYPES.GetActivitiesController) getActivitiesController: Controller,
-        @inject(TYPES.GetActivityController) getActivityController: Controller
-    ){
-        this.topPath = "/activities";
-        this.getActivitiesController = getActivitiesController;
-        this.getActivityController = getActivityController;
-    }
+	constructor(
+		@inject(TYPES.GetActivitiesController) getActivitiesController: Controller,
+		@inject(TYPES.GetActivityController) getActivityController: Controller
+	) {
+		this.topPath = '/activities';
+		this.getActivitiesController = getActivitiesController;
+		this.getActivityController = getActivityController;
+	}
 
-    getTopRoute(): string{
-        return this.topPath;
-    }
+	getTopRoute(): string {
+		return this.topPath;
+	}
 
-    getRoutes(): Route[]{
-        return [
-            { 
-                path: "/", 
-                controller: this.getActivitiesController, 
-                method: "get" 
-            },
-            { 
-                path: "/:activityId", 
-                controller: this.getActivityController, 
-                method: "get" },
-            /*{ 
-                path: "/", 
-                controller: "createActivityController", 
-                method: "post" 
-            }*/
-        ]
-    }
+	getRoutes(): Route[] {
+		return [
+			{
+				path: '/',
+				controller: this.getActivitiesController,
+				method: 'get'
+			},
+			{
+				path: '/:activityId',
+				controller: this.getActivityController,
+				method: 'get'
+			},
+		];
+	}
 }
 
-export { ActivityRouter }
+export { ActivityRouter };
